dbfb76c96cc32752eacdbd9cfcb7ce428e708ef1
callumvincent <22cv2823@barton.ac.uk>
2025-12-02 18:01:42 +0000

    started reload when there is an error

--- dialogueSystem/endSessionManager/fileSaver.js
+++ dialogueSystem/endSessionManager/fileSaver.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { getSession, deleteSession } from "../../liveSessionState/sessionState.js";
+import { sessionGateRouter } from "../dataGateway/gateRouter.js";
 
 
 export default function fileSaver() {
@@ -15,11 +16,23 @@ export default function fileSaver() {
     id: currentSession.id,
     sessionLog: currentSession.sessionLog
   };
-
+  if (currentSession.error == true) {
+    ErrorReload();
+  }
   // appends the current session data to all previous sessions
   fs.appendFileSync(sessionFilePath, JSON.stringify(sessionData) + "\n", "utf-8");
 
    // deletes the session from memory after saving
   deleteSession();
+  
+}
 
+
+export function ErrorReload() {
+  const sessionFilePath = path.join(process.cwd(), 'log.json');
+  const jsonData = fs.readFileSync(sessionFilePath, 'utf-8');
+  const data = JSON.parse(jsonData);
+  const latestSave = data[data.length - 1];
+  sessionGateRouter(latestSave.sessionLog[sessionLog.length - 1])
+  
 }
