661a9a429aaaf5f8cd3c03cf46fdfdb8e6d9b1b6
JoshKing85 <joshking1985@hotmail.co.uk>
2025-12-02 16:44:57 +0000

    small edit

--- dialogueSystem/errorHandler/errorSwitch.js
+++ dialogueSystem/errorHandler/errorSwitch.js
@@ -1,10 +1,15 @@
+import { inputError, noReturnMsg } from "./errorHelpers.js";
+
+
 export function errorSwitch(messageEnvelope, history){
-    const swtichHist = history[-1].errorCount
     const switchErrMsg = messageEnvelope.response
 
     switch(switchErrMsg){
 
         case 'no input':
-             return inputError(messageEnvelope)
+             return inputError(messageEnvelope, history)
+        
+        case 'no return me':
+            return noReturnMsg(messageEnvelope, history)
     }
 }
---
+++ dialogueSystem/errorHandler/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+noReturnMsg(messageEnvelope, history)
