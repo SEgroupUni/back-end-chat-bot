5eea976934667fb5d720d3c059f667b3dca511b8
Dom056 <dominicdawson99@gmail.com>
2025-12-03 18:59:24 +0000

    fixed

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,32 +1,22 @@
 import express from 'express';
-import {
-    sessionGateRouter,
-} from '../../dialogueSystem/dataGateway/gateRouter.js';
-import { ErrorReload } from "../../dialogueSystem/endSessionManager/fileSaver.js";
+import { sessionGateRouter } from '../../dialogueSystem/dataGateway/gateRouter.js';
 
 const router = express.Router();
 
-router.post("/chat", async (req, res) => {
-    const userInput = req.body.message;
+router.post('/', async (req, res, next) => {
+  try {
+    const { userInput } = req.body;
 
     const result = await sessionGateRouter(userInput);
 
-    if (result.error) {
-        console.log("Restarting session after error…");
-
-        const restarted = await ErrorReload();
-
-        return res.json({
-            response: restarted.response,
-            userPrompt: restarted.userPrompt
-        });
-    }
-
     res.json({
-        response: result.response,
-        userPrompt: result.userPrompt
+      response: result.response,
+      userPrompt: result.userPrompt
     });
+
+  } catch (err) {
+    next(err);
+  }
 });
 
-export default router;   
-
+export default router;
